ef17cc4987a7e88af6ddfcb9d0ebdcdd963f3f8b
Lyubomr <s.lyubomyr@gmail.com>
2016-01-12 15:32:50 -0500

    Add 2nd excersice

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+
+# First problem.
+puts "Please eneter your tip %"
+tip = gets.chomp.to_f
+tip = tip / 100
+bill_value = 55
+puts " Your tip total is #{tip * bill_value}"
+
+
+# Second problem
+
+puts "How many blisters do I have on my fingers?" + 5.to_s
+
+# Third problem
+puts "The sum of 45628 and 7839 = #{45628*7839}"
+
+# Fourth problem
+puts (10 < 20 && 30 < 20) || !(10 == 11) #true
